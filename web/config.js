0ee2ab52c4babe19a75620e1a55d5deca2f7d5b1
Marcin Halczuk <halczukmarcin@gmail.com>
2017-05-30 18:51:09 +0200

    Dodany endpoint zwracajacy sklepy w poblizu

--- web/config.js
+++ web/config.js
@@ -3,5 +3,6 @@ var config = {};
 config.PORT = 3000;
 config.DB_PORT = 27017;
 config.GOOGLE_API_KEY = "AIzaSyCgR_7bytuWpa9tlhiZqizzdOUlows6Emc";
+config.SHOP_NEARBY = 5;
 
 module.exports = config;
